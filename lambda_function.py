94604b884242eacdf45a49298a51b5fcda3534a1
Yukitaka Maeda <yumaeda@gmail.com>
2019-03-12 00:39:00 +0900

    Update lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,3 @@
-import boto3
 import os
 import json
 import sys
@@ -6,18 +5,24 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'vendors'))
 from MyQR import myqr
 
 def lambda_handler(event, context):
-    url = 'https://github.com/yumaeda/python-qr'
-    srcImg = 'src.jpg'
     OUT_FILE = 'target.gif'
     OUT_DIR = '/tmp'
     BUCKET = 'yumaeda'
 
+    text = 'https://github.com/yumaeda/python-qr'
+    if 'text' in event:
+        text = event['text']
+
+    img = 'src.jpg'
+    if 'img' in event:
+        img = event['img']
+
     try:
         version, level, qr_name = myqr.run(
-            url,
+            text,
             version = 10,
             level = 'H',
-            picture = srcImg,
+            picture = img,
             colorized = True,
             contrast = 1.0,
             brightness = 1.0,
@@ -25,6 +30,7 @@ def lambda_handler(event, context):
             save_dir = OUT_DIR
         )
 
+        import boto3
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(BUCKET)
         bucket.upload_file(
@@ -37,5 +43,6 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('SUCCESS')
+        'img': json.dumps(img),
+        'text': json.dumps(text)
     }
